4eab36b7ef30180c5b539f3f7d5a630135a9226d
Tinahiii <116031354+Tinahiii@users.noreply.github.com>
2025-08-31 18:58:14 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -14,9 +14,7 @@ function App() {
     <>
       <div>
          <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <Footer />
+      
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -24,7 +22,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+       <Header />
       <h1>Vite + React</h1>
+        <MainContent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -36,6 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <Footer />
     </>
   )
 }

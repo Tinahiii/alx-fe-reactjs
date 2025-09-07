f1311f5a0e7beec4915191de47783202af993cf5
Tinahiii <116031354+Tinahiii@users.noreply.github.com>
2025-09-07 15:47:41 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -5,7 +5,7 @@ return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4', minHeight: '200px' }}>
       <h2 style={{ textAlign: 'center', marginBottom: '15px' }}>About My Favorite Cities</h2>
       <p style={{ lineHeight: '1.6', textAlign: 'justify' }}>
-        Cities are full of history, culture, and diversity. Each city has its own unique vibe and story to tell.
+I love to visit New York, Paris, and Tokyo.
       </p>
 </main>
 );

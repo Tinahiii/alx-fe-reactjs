e20d64eb21a2dc791daadf790cf81781fa3bb5f7
Tinahiii <116031354+Tinahiii@users.noreply.github.com>
2025-09-19 23:45:36 +0300

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -12,9 +12,16 @@ import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  
+  
 
   return (
     <>
+        <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
       <div>
          <WelcomeMessage />
         <Userprofile />

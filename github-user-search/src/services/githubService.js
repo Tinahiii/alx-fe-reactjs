0d4c08e9f015356bcc654e29e74ec1612cf0058b
Tinahiii <116031354+Tinahiii@users.noreply.github.com>
2025-09-21 15:15:25 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-// Function to fetch GitHub user data
-export async function fetchUserData(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
+// Advanced search function
+export async function fetchAdvancedUserData({ username, location, minRepos }) {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos} `;
+
+  const response = await axios.get(
+    `https://api.github.com/search/users?q=${query.trim()}`
+  );
+
   return response.data;
 }

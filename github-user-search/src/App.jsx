b144d5f4d6f28aaaec8453e9f399330eebfd8303
Tinahiii <116031354+Tinahiii@users.noreply.github.com>
2025-09-21 15:26:26 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>GitHub User Search</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+
       <Search />
     </div>
   );
